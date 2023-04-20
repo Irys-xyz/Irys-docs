e2ca4e4483237a7f8b78e3bb87adf935e69ae53f
Luke Cassady-Dorion <luke@SPStories.com>
2023-04-20 09:21:21 +0100

    updated footer links

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,43 +1,39 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from "react";
 
-import { DevIcon } from '../svg'
+import { DevIcon } from "../svg";
 
 const useButton = () => {
-  return {}
-}
+	return {};
+};
 
 export enum ButtonScheme {
-  black = 'black',
-  white = 'white'
+	black = "black",
+	white = "white",
 }
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  scheme?: ButtonScheme
-  children: ReactNode
+	scheme?: ButtonScheme;
+	children: ReactNode;
 }
 
-const Button = ({
-  scheme = ButtonScheme.black,
-  children,
-  className,
-  ...props
-}: ButtonProps) => {
-  const { } = useButton()
+const Button = ({ scheme = ButtonScheme.black, children, className, ...props }: ButtonProps) => {
+	const {} = useButton();
 
-  return (
-    <>
-      <button
-        className={`text-md z-50 flex items-center gap-2 rounded-full px-4 py-3 font-robotoMono uppercase hover:font-bold lg:px-6 lg:py-5 ${{
-            [ButtonScheme.black]: 'bg-black text-white',
-            [ButtonScheme.white]: 'bg-white text-black'
-          }[scheme]
-          } ${className}}`}
-        {...props}
-      >
-        {children}
-      </button>
-    </>
-  )
-}
+	return (
+		<>
+			<button
+				className={`text-md z-50 flex items-center gap-2 rounded-full px-4 py-3 font-robotoMono uppercase hover:font-bold lg:px-6 lg:py-5 ${
+					{
+						[ButtonScheme.black]: "bg-black text-white",
+						[ButtonScheme.white]: "bg-white text-black",
+					}[scheme]
+				} ${className}}`}
+				{...props}
+			>
+				{children}
+			</button>
+		</>
+	);
+};
 
-export default Button
+export default Button;
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,136 +1,133 @@
-import {
-  DiscordIcon,
-  GitIcon,
-  MediumIcon,
-  MirrorIcon,
-  TelegramIcon,
-  TwitterIcon
-} from '../svg'
+import { DiscordIcon, GitIcon, MediumIcon, MirrorIcon, TelegramIcon, TwitterIcon } from "../svg";
 
-import FooterDesktop from '../NewFooter/FooterDesktop'
-import FooterMobile from '../NewFooter/FooterMobile'
+import FooterDesktop from "../NewFooter/FooterDesktop";
+import FooterMobile from "../NewFooter/FooterMobile";
 
 const Footer = () => {
+	const DEFINITIONS = [
+		{
+			title: "OVERVIEW",
+			options: [
+				{
+					name: "About Bundlr",
+					href: "/overview/about-bundlr",
+					newTab: false,
+				},
+				{
+					name: "Permanent Decentralized Storage",
+					href: "/overview/solutions/permanent-storage",
+					newTab: false,
+				},
+				{
+					name: "Proof of Provenance",
+					href: "/overview/solutions/proof-of-provenance",
+					newTab: false,
+				},
+				{
+					name: "General FAQs",
+					href: "/faqs/general-faq",
+					newTab: false,
+				},
+				{
+					name: "Developer FAQs",
+					href: "/faqs/dev-faq",
+					newTab: false,
+				},
+			],
+		},
+		{
+			title: "DEVELOPER DOCS",
+			options: [
+				{
+					name: "Quickstart",
+					href: "/developer-docs/quickstart",
+					newTab: false,
+				},
+				{
+					name: "SDK",
+					href: "/developer-docs/sdk",
+					newTab: false,
+				},
+				{
+					name: "CLI",
+					href: "/developer-docs/cli",
+					newTab: false,
+				},
+			],
+		},
+		{
+			title: "HANDS ON",
+			options: [
+				{
+					name: "Tutorials",
+					href: "/hands-on/tutorials",
+				},
+				{
+					name: "Developer Quests",
+					href: "/hands-on/quests",
+				},
+			],
+			footer: (
+				<div className="mt-auto flex items-center gap-[21px] pb-[40px]">
+					<a
+						href="https://medium.com/bundlr-network"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<MediumIcon />
+					</a>
+					<a
+						href="https://mirror.xyz/0x9AbB09BF9F58E72A532E859d798eB4E8e10A35e1"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<MirrorIcon className="w-6 h-6 text-black" />
+					</a>
+					<a
+						href="https://twitter.com/BundlrNetwork"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<TwitterIcon />
+					</a>
+					<a
+						href="https://discord.gg/bundlr"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<DiscordIcon />
+					</a>
+					<a
+						href="https://github.com/Bundlr-Network"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<GitIcon />
+					</a>
+					<a
+						href="https://t.me/bundlr"
+						target={"_blank"}
+						rel="nofollow noreferrer"
+						className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
+					>
+						<TelegramIcon />
+					</a>
+				</div>
+			),
+		},
+	];
 
-  const DEFINITIONS = [
-    {
-      title: "LEARN",
-      options: [
-        {
-          name: "What is Bundlr",
-          href: "/overview",
-          newTab: false,
-        },
-        {
-          name: "Transaction Lifecycle",
-          href: "/learn/transaction-lifecycle",
-          newTab: false,
-        },
-        {
-          name: "Proof of Provenance",
-          href: "/learn/proof-of-provenance",
-          newTab: false,
-        },
-        {
-          name: "FAQs",
-          href: "/FAQs/general-faq",
-          newTab: false,
-        },
-      ],
-    },
-    {
-      title: "START CODING",
-      options: [
-        {
-          name: "SDK",
-          href: "/sdk/basic-features",
-          newTab: false,
-        },
-        {
-          name: "WebBundlr",
-          href: "/sdk/bundlr-in-the-browser",
-          newTab: false,
-        },
-        {
-          name: "CLI",
-          href: "/CLI/installation-setup",
-          newTab: false,
-        },
-      ],
-    },
-    {
-      title: "DIVE DEEP",
-      options: [
-        {
-          name: "Recipes",
-          href: "/recipes/bundlr-react",
-        },
-        {
-          name: "Tutorials",
-          href: "/recipes/bundlr-react",
-        },
-      ],
-      footer: (
-        <div className="mt-auto flex items-center gap-[21px] pb-[40px]">
-          <a
-            href="https://medium.com/bundlr-network"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <MediumIcon />
-          </a>
-          <a
-            href="https://mirror.xyz/0x9AbB09BF9F58E72A532E859d798eB4E8e10A35e1"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <MirrorIcon className="w-6 h-6 text-black" />
-          </a>
-          <a
-            href="https://twitter.com/BundlrNetwork"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <TwitterIcon />
-          </a>
-          <a
-            href="https://discord.gg/bundlr"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <DiscordIcon />
-          </a>
-          <a
-            href="https://github.com/Bundlr-Network"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <GitIcon />
-          </a>
-          <a
-            href="https://t.me/bundlr"
-            target={"_blank"}
-            rel="nofollow noreferrer"
-            className="transition-all hover:rotate-6 hover:scale-105 text-black hover:text-black"
-          >
-            <TelegramIcon />
-          </a>
-        </div>
-      ),
-    },
-  ];
+	return (
+		<>
+			<FooterMobile data={DEFINITIONS} />
+			<FooterDesktop data={DEFINITIONS} />
+		</>
+	);
+};
 
-  return (
-    <>
-      <FooterMobile data={DEFINITIONS} />
-      <FooterDesktop data={DEFINITIONS} />
-    </>
-  )
-}
-
-export default Footer
+export default Footer;
