c696b9c4e4954ce8f8593513818b0e66bda2e0f2
Luke Cassady-Dorion <luke@SPStories.com>
2023-04-25 09:19:43 +0100

    reworked lens quest

--- next.config.js
+++ next.config.js
@@ -1,39 +1,40 @@
 const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias["@sentry/node"] = "@sentry/browser";
-    }
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.tsx",
+	defaultShowCopyCode: true,
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.alias["@sentry/node"] = "@sentry/browser";
+		}
 
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      use: [
-        {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-          },
-        },
-      ],
-    });
+		config.module.rules.push({
+			test: /\.tsx?$/,
+			use: [
+				{
+					loader: "ts-loader",
+					options: {
+						transpileOnly: true,
+					},
+				},
+			],
+		});
 
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    });
+		config.module.rules.push({
+			test: /\.css$/,
+			use: ["style-loader", "css-loader", "postcss-loader"],
+		});
 
-    config.resolve.extensions.push(".ts", ".tsx");
+		config.resolve.extensions.push(".ts", ".tsx");
 
-    return config;
-  },
+		return config;
+	},
 });
 
 module.exports = {
-  ...withNextra(),
-  images: {
-    unoptimized: true,
-  },
+	...withNextra(),
+	images: {
+		unoptimized: true,
+	},
 };
 
 // If you have other Next.js configurations, you can pass them as the parameter:
