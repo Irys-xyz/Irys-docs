9d39faeb44237d24722cfd19db150aff82383f4b
Luke Cassady-Dorion <luke@SPStories.com>
2023-06-08 14:52:13 +0200

    added directions

--- components/ButtonBar/ButtonBar.tsx
+++ components/ButtonBar/ButtonBar.tsx
@@ -7,12 +7,12 @@ interface ButtonBarProps {
 	contractAddress: string;
 	tokenId: number;
 }
-//
+
 const ButtonBar: React.FC<ButtonBarProps> = ({ submitUrl, contractAddress, tokenId }) => {
 	const successMessage = `Congratulations on finishing the quest! Your NFT has been claimed, you can view it on <a class="underline decoration-white" href="https://testnets.opensea.io/assets/mumbai/0x6a954b537ebd8477320246d47b9d6eee51b49f0b/107" target="_blank">Opensea</a>`;
 
-	const [message, setMessage] = useState<string>("");
-
+	const [message, setMessage] = useState<string>("This is a test");
+	const [claimButtonText, setClaimButtonText] = useState<string>("Claim NFT!");
 	return (
 		<div className="flex flex-col">
 			<div className="flex flex-row gap-4 mt-10">
@@ -24,11 +24,11 @@ const ButtonBar: React.FC<ButtonBarProps> = ({ submitUrl, contractAddress, token
 					onSuccess={() => setMessage(successMessage)}
 					onError={(error) => setMessage(error.message)}
 				>
-					Claim NFT!
+					{claimButtonText}
 				</Web3Button>
 			</div>
 			<div
-				className="mt-5 text-left text-red-500"
+				className="mt-5 text-left"
 				dangerouslySetInnerHTML={{ __html: message }}
 			/>
 		</div>
