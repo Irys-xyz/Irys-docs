14618c2f8b46230b8a91724ea96874356dc54da5
Luke Cassady-Dorion <luke@SPStories.com>
2023-09-03 08:45:04 +0700

    added quest 3 claim button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,21 +6,30 @@ import "@fontsource/roboto-mono/700.css";
 
 import Analytics from "../components/Analytics";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useGoogleTagManager from "../hooks/useGoogleTagManager";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner | null>(null);
+
 	useEffect(() => {
 		localStorage.setItem("theme", "dark");
+	}, []);
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setSigner(new ethers.providers.Web3Provider(window.ethereum).getSigner());
+		}
+		console.log("signer", signer);
 	}, []);
 
 	useGoogleTagManager();
 
 	return (
 		<>
-			<ThirdwebProvider activeChain="polygon" clientId="8026fd043736df9e5a4bfa9a57e5f66c">
+			<ThirdwebProvider activeChain={"polygon"} signer={signer} clientId="8026fd043736df9e5a4bfa9a57e5f66c">
 				<Component {...pageProps} />
 				<Analytics />
 			</ThirdwebProvider>
