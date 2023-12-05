41a06c0ce165e8bb64942e32206982e9e6ba3723
TheHoltz <holtzs.william@gmail.com>
2023-12-05 08:52:30 -0300

    feat: :sparkles: eth as default option

--- components/UploadFeeCalculator/UploadFeeCalculator.tsx
+++ components/UploadFeeCalculator/UploadFeeCalculator.tsx
@@ -127,7 +127,7 @@ const UploadFeeCalculator: React.FC = () => {
 	];
 
 	const [selectedFileUnit, setSelectedFileUnit] = useState(FILE_UNITS[0]);
-	const [currency, setCurrency] = useState(tokenOptions[6]);
+	const [currency, setCurrency] = useState(tokenOptions[5]);
 	const [droppedFiles, setDroppedFiles] = useState<FileWithPreview[]>([]);
 	const [items, setItems] = useState<{ name: string; value: string }[]>([]);
 	const [customSize, setCustomSize] = useState(0);
