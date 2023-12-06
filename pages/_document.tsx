c72f03517b345b80ab352576da561098e790bf85
TheHoltz <holtzs.william@gmail.com>
2023-12-06 11:29:33 -0300

    feat: :sparkles: updated metatags to opengraph only (supported by twitter)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,25 +9,19 @@ const Document = () => {
 					rel="stylesheet"
 				/>
 
-				<meta property="og:url" content="https://irys.xyz" />
+				<meta property="og:url" content="https://docs.irys.xyz" />
 				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Irys" />
+				<meta property="og:title" content="Irys | Docs" />
 				<meta
 					property="og:description"
 					content="Unlock the full potential of Irys' technology with our developer resources. Use our SDK and start storing data permanently with 3-4 lines of code."
 				/>
-				<meta property="og:image" content="https://irys.xyz/new/assets/shared/open-graph.png" />
+				<meta
+					property="og:image"
+					content="https://irys.xyz/new/assets/shared/open-graph.png"
+				/>
 
 				<meta name="twitter:card" content="summary_large_image" />
-				<meta property="twitter:domain" content="irys.xyz" />
-				<meta property="twitter:url" content="https://irys.xyz" />
-				<meta name="twitter:title" content="Irys | Home" />
-				<meta
-					name="twitter:description"
-					content="Unlock the full potential of Irys's technology with our developer resources. Use our SDK and start storing data permanently with 3-4 lines of code."
-				/>
-				<meta name="twitter:image" content="https://irys.xyz/new/assets/shared/open-graph.png" />
-
 				{/* // favicon */}
 				<link rel="icon" href="/img/favicon.svg" />
 			</Head>
