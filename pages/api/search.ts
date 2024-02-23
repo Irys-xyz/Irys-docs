05deb09309f8d44c07523a9c9dcf944ead0f9409
TheHoltz <holtzs.william@gmail.com>
2024-02-22 22:26:43 -0300

    feat: :sparkles: added cors headers

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -3,6 +3,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 import FlexSearch from "flexsearch";
 import db from "./db.json";
 
+const allowCors = (res: NextApiResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  ); // Allowed methods
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); // Allowed headers
+};
+
 const index = new FlexSearch.Index({
   tokenize: "forward",
   resolution: 3,
@@ -21,6 +33,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(400).json({ error: "Query parameter is required" });
     return;
   }
+
+  allowCors(res);
 
   const results = index.search(q, { limit: 10 });
 
