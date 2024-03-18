3757fae144f7bab64cf3322e4271f1802b74a2ca
TheHoltz <holtzs.william@gmail.com>
2024-03-18 16:38:36 -0300

    feat: :sparkles: search api update

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -3,6 +3,36 @@ import { NextApiRequest, NextApiResponse } from "next";
 import FlexSearch from "flexsearch";
 import db from "./db.json";
 
+function findSnippetAroundKeyword(text: string, keyword: string) {
+  const CHARACTER_AROUND = 20; // Adjust to use characters instead of words around
+
+  // Convert text to lowercase for case-insensitive search
+  const lowerText = text.toLowerCase();
+  const lowerKeyword = keyword.toLowerCase();
+
+  let startIndex = lowerText.indexOf(lowerKeyword);
+
+  if (startIndex === -1) {
+    return "Keyword not found";
+  }
+
+  // Calculate start and end positions for the snippet
+  let snippetStart = Math.max(startIndex - CHARACTER_AROUND, 0);
+  let snippetEnd = Math.min(
+    startIndex + keyword.length + CHARACTER_AROUND,
+    text.length
+  );
+
+  // Determine whether ellipses should be added before/after the snippet
+  let snippetPrefix = snippetStart > 0 ? "..." : "";
+  let snippetSuffix = snippetEnd < text.length ? "..." : "";
+
+  // Extract and return the snippet with potential ellipses
+  return (
+    snippetPrefix + text.substring(snippetStart, snippetEnd) + snippetSuffix
+  );
+}
+
 const allowCors = (res: NextApiResponse) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
   res.setHeader(
@@ -42,6 +72,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return {
       path: db[id].path,
       description: db[id].description,
+      preview: findSnippetAroundKeyword(db[id].content, q).replace(
+        new RegExp(q, "gi"),
+        (match: string) => `<b className="font-bold"><u>${match}</u></b>`
+      ),
     };
   });
 
