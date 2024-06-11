12d8fc6b2de07a01f9b8d25c0a3cc5d8b72da044
TheHoltz <holtzs.william@gmail.com>
2024-06-10 21:05:57 -0300

    fix: :bug: removed unused prop

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,70 +1,71 @@
 import Footer from "./components/Footer";
 import IrysLogoText from "./components/svg/IrysLogoText";
-import { useConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
 
 const FooterWrapper = () => {
-	const router = useRouter();
-	return <Footer noPadding={router.asPath !== "/"} />;
+  return <Footer />;
 };
 
 export default {
-	nextThemes: {
-		defaultTheme: "dark",
-	},
-	themeSwitch: {
-		useOptions() {
-			return {
-				dark: "Dark",
-			};
-		},
-	},
-	editLink: {
-		component: () => null,
-	},
-	feedback: {
-		content: () => null,
-	},
-	sidebar: {
-		toggleButton: true,
-	},
-	logo: <IrysLogoText width={65} />,
-	project: {
-		link: "https://github.com/Irys-xyz/",
-	},
-	footer: {
-		component: FooterWrapper,
-	},
-	banner: {},
-	useNextSeoProps() {
-		const { asPath } = useRouter();
-		if (asPath !== "/") {
-			return {
-				titleTemplate: "Irys | %s",
-			};
-		} else {
-			return {
-				titleTemplate: "Irys | Docs",
-			};
-		}
-	},
-	head: () => {
-		return (
-			<>
-				<meta property="og:site_name" content="Irys" />
-				<meta property="og:url" content="https://docs.irys.xyz" />
-				<meta property="og:type" content="website" />
-				<meta property="og:image" content="https://irys.xyz/new/assets/shared/open-graph.png" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<script type="application/ld+json">
-					{`{
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        dark: "Dark",
+      };
+    },
+  },
+  editLink: {
+    component: () => null,
+  },
+  feedback: {
+    content: () => null,
+  },
+  sidebar: {
+    toggleButton: true,
+  },
+  logo: <IrysLogoText width={65} />,
+  project: {
+    link: "https://github.com/Irys-xyz/",
+  },
+  footer: {
+    component: FooterWrapper,
+  },
+  banner: {},
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "Irys | %s",
+      };
+    } else {
+      return {
+        titleTemplate: "Irys | Docs",
+      };
+    }
+  },
+  head: () => {
+    return (
+      <>
+        <meta property="og:site_name" content="Irys" />
+        <meta property="og:url" content="https://docs.irys.xyz" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://irys.xyz/new/assets/shared/open-graph.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <script type="application/ld+json">
+          {`{
   "@context" : "https://schema.org",
   "@type" : "WebSite",
   "name" : "Irys",
   "url" : "https://docs.irys.xyz"
 }`}
-				</script>
-			</>
-		);
-	},
+        </script>
+      </>
+    );
+  },
 };
